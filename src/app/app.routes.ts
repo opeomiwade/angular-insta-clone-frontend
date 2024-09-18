e7090f7a326eff832ec5bae902dae6b82249b59d
Ope Omiwade <opeomiwade21@gmail.com>
2024-09-18 07:26:39 +0100

    login page

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+
+export const routes: Routes = [
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+];
